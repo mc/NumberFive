e8dda015a505b564cd24401918f1f7b9c7e07a18
Rico Gloeckner <many@argabuthon.ukeer.de>
2011-06-15 11:33:38 +0200

    no5; loeschbegruendung korrekt loggen no5; kommentare eigefuegt ;)

--- no5-delete.rb
+++ no5-delete.rb
@@ -5,43 +5,61 @@ require '/home/many/wiki.pp/NumberFive/.conf/credentials'
 
 # Sieht komplizierter aus als es ist.
 
+# Open Wiki Connection
 wiki = Api.new(nil, nil, WIKI_USER, WIKI_SERVER, WIKI_APIURL)
 wiki.login(WIKI_PASSWORD)
 
+# These Pages are never deleted
 exceptions = [ "Kategorie:URV", "Vorlage:Schnelllöschen" ]
 
-timeStart = Time.now - (13 * 24 * 60 * 60)
+# Calculate DAYS days in seconds and put wiki-compatible time string into var
+timeStart = Time.now - (14 * 24 * 60 * 60)
 timeStartstring = timeStart.strftime("%Y-%m-%dT%H:%M:%SZ")
 
+# Query the List of pages thats gonna be deleted
 toDelete = wiki.query_list_categorymembers(:cmtitle => "Kategorie:Löschen", 
 	:cmlimit => 100, :cmprop => "timestamp|ids|title", :cmsort => "timestamp", 
 	:cmdir => "desc", :cmstart => timeStartstring)
 
 deleted = 0
+
+# are there actually pages to delete?
 cm = toDelete["query"]["categorymembers"]
 if cm.empty?
 	exit 0
 end
 
+
 begin
   loeschlog = Page.new("Benutzer:NumberFive/Loeschprotokoll", wiki)
   loeschtext = ""
+
+  # iterate through list of categorymembers
   cm["cm"].each do |cm|
     if cm.is_a?(Hash) and ! exceptions.include?(cm["title"])
       page = Page.new(cm["title"], wiki)
       content = page.get
       reason = content["content"].match(/.*\{\{(SLA|(schnell)?L..?schen)\|?(.*?)?\}\}.*/i)
+
       if reason != nil
+          # normalize category
+          titel = cm["title"]
+	  if cm["title"].match(/^Kategorie:/)
+	    titel = ":" + cm["title"]
+          end
+
+          # provide a deletion-log
 	  if reason[3].nil?
-            loeschtext = loeschtext + "<br/> [[" + cm["title"] + "]]: " + "(Keine Loeschbegruendung angegeben)"
+            loeschtext = loeschtext + "<br/> [[#{titel}]]: " + "(Keine Loeschbegruendung angegeben)"
 	  else
-            loeschtext = loeschtext + "<br/> [[" + cm["title"] + "]]: " + reason[3]
+            loeschtext = loeschtext + "<br/> [[#{titel}]]: " + reason[3]
 	  end
         page.delete("Automatische Loeschung nach 14 Tagen per " +
           "[[Benutzer:NumberFive/Loeschbot]]: " + reason[3])
       else
         loeschtext = "<br/> :: [[" + cm["title"] + "]] -- '''Fehler beim Loeschen!"
-      end
+      end # if reason != nil
+
     end
 
     deleted = deleted + 1
@@ -51,6 +69,7 @@ begin
   end
 
 ensure
+  # in any case, write the log at the wiki
   if loeschtext != ""
     loeschtext = "\n== " + Time.now.to_s + " == \n" + loeschtext
     loeschlog.append(loeschtext, "Loeschlog", false, true)
